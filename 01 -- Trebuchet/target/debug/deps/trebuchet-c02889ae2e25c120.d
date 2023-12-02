1dc1372c9dad55b2738099dd8b6c1baf9385a64f
Arlen <arlen.brower@curtin.edu.au>
2023-12-02 14:46:29 +0800

    Cleanup

--- 01 -- Trebuchet/target/debug/deps/trebuchet-c02889ae2e25c120.d
+++
@@ -1,5 +0,0 @@
-/home/arlen/projects/advent-of-code/aoc-2023/01 -- Trebuchet/target/debug/deps/trebuchet-c02889ae2e25c120.rmeta: src/main.rs
-
-/home/arlen/projects/advent-of-code/aoc-2023/01 -- Trebuchet/target/debug/deps/trebuchet-c02889ae2e25c120.d: src/main.rs
-
-src/main.rs:
--- 01 -- Trebuchet/target/debug/trebuchet.d
+++
@@ -1,1 +0,0 @@
-/home/arlen/projects/advent-of-code/aoc-2023/01\ --\ Trebuchet/target/debug/trebuchet: /home/arlen/projects/advent-of-code/aoc-2023/01\ --\ Trebuchet/src/main.rs
