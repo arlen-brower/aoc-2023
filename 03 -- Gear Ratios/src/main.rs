7e8d4a8d9df931c1259746bfb5c06fdf4cc5857c
Arlen Brower <arlen.brower@curtin.edu.au>
2023-12-03 17:55:56 +0800

    Day 3 -- Reading the file

--- 03 -- Gear Ratios/src/main.rs
+++ 03 -- Gear Ratios/src/main.rs
@@ -1,12 +1,22 @@
-
 use std::env;
-use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
 
-    let mut contents =
-        fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let f = BufReader::new(File::open(file_path).unwrap());
 
+    let arr: Vec<Vec<char>> = f
+        .lines()
+        .map(|l| l.unwrap().chars().map(|c| c).collect())
+        .collect();
+
+    for line in arr {
+        for c in line {
+            print!("{c}");
+        }
+        println!();
+    }
 }
