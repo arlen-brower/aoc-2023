97599783e1b4e2ad31c85897ef70131d73ebff29
Arlen <arlen.brower@curtin.edu.au>
2023-12-04 01:15:26 +0800

    Day 3 -- Parts one and two

--- 03 -- Gear Ratios/src/main.rs
+++ 03 -- Gear Ratios/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs::File;
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader};
 
 fn main() {
@@ -17,6 +18,12 @@ fn main() {
     let cols = arr[0].len();
 
     let mut parts: Vec<i32> = Vec::new();
+    // I basically just made myself a mini-object for the Gears :P
+    // (row, column, gear_id, part_number)
+    // Gear ID was really just there in case there are part_numbers that are identical
+    // ... but I don't think there were.
+    let mut gears: Vec<(usize, usize, i32, i32)> = Vec::new();
+    let mut gear_id: i32 = 0;
     let mut new_digit: bool = true;
     let mut has_symbol: bool = false;
     let mut left_idx = 0;
@@ -36,16 +43,12 @@ fn main() {
                 num_str.push(arr[r][c]);
 
                 if c != cols-1 && arr[r][c+1].is_digit(10) {
+                    // I thought I was going to do something here. I did not. :)
                 }
                 else {
                     // Digit parsed. Check for symbols.
                     right_idx = c; // Last known digit.
                     new_digit = true;
-
-                    // for i in left_idx..right_idx+1 {
-                    //     print!("{}", arr[r][i]);
-                    // }
-                    // println!();
 
                     // Bounds checking stuff
                     let mut lbr = 0;
@@ -73,6 +76,10 @@ fn main() {
                             let check_char = arr[search_rows][search_cols];
                             if !check_char.is_digit(10) && check_char != '.' {
                                 has_symbol = true;
+                                if check_char == '*' {
+                                    gears.push((search_rows, search_cols, gear_id, num_str.parse().unwrap()));
+                                    gear_id = gear_id + 1;
+                                }
                             }
                         }
                     }
@@ -91,13 +98,30 @@ fn main() {
         } // End columns
     }// End rows
     
-    let mut sum = 0;
 
-    for n in parts {
-        sum = sum + n;
-        // print!("{n} ");
+    // Messy business with me learning Rust collections!
+    let mut gear_set = HashMap::new();
+    let mut ratios: Vec<i32> = Vec::new();
+
+    for (gr, gc, gid, part_no) in gears {
+        println!("Gear {gid} -- {gr},{gc} : {part_no}");
+        if gear_set.contains_key(&(gr,gc)){
+            match gear_set.get(&(gr,gc)) {
+                Some(pno ) => ratios.push(pno*part_no),
+                None => println!("Oh no")
+            }
+        }
+        else {
+            gear_set.insert((gr,gc), part_no);
+        }
+
     }
+
+    let sum: i32 = parts.iter().sum();
+    let ratio_sum: i32 = ratios.iter().sum();
+
     println!();
-    println!("{sum}");
+    println!("Part One: {sum}");
+    println!("Part Two: {ratio_sum}");
 
 }
