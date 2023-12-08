248f62a0a02c7d8562411676cdf2c1754a64a62f
Arlen <arlen.brower@curtin.edu.au>
2023-12-08 17:20:59 +0800

    Day 2 -- Edits for Part 2

--- 08 -- Haunted Wasteland/src/main.rs
+++ 08 -- Haunted Wasteland/src/main.rs
@@ -14,6 +14,7 @@ fn main() {
     let (instr_str, nodes) = contents.split_once("\n\n").unwrap();
     let mut instructions = instr_str.chars().cycle();
     let mut node_map: HashMap<&str, (&str, &str)> = HashMap::new();
+    let mut starts: Vec<&str> = Vec::new();
 
     for line in nodes.lines() {
         let mut split_iter = line.split(char::is_whitespace);
@@ -21,21 +22,46 @@ fn main() {
         let l = split_iter.next().unwrap();
         let r = split_iter.next().unwrap();
         node_map.insert(k, (l, r));
+
+        if k.chars().nth(2).unwrap() == 'A' {
+            starts.push(k);
+            println!("{k}");
+        }
     }
 
-    let mut cur_node: &str = "AAA";
-    let mut steps = 0;
+    let mut step_v: Vec<i64> = Vec::new();
+    for n in starts {
+        let mut cur_node: &str = n;
+        let mut steps = 0;
 
-    while cur_node != "ZZZ" {
-        println!("{cur_node}");
-        let (l, r) = node_map[cur_node];
-        cur_node = match instructions.next().unwrap() {
-            'L' => l,
-            'R' => r,
-            _ => panic!(),
-        };
-        steps += 1;
+        while cur_node.chars().nth(2).unwrap() != 'Z' {
+            // println!("{cur_node}");
+            let (l, r) = node_map[cur_node];
+            cur_node = match instructions.next().unwrap() {
+                'L' => l,
+                'R' => r,
+                _ => panic!(),
+            };
+            steps += 1;
+        }
+        step_v.push(steps);
+        println!("\n{n}:\n{steps}");
     }
+    println!("\nPart 2)\n{}", lcm(&step_v[..]));
+}
 
-    println!("\nPart 1)\n{steps}");
+fn lcm(nums: &[i64]) -> i64 {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    let a = nums[0];
+    let b = lcm(&nums[1..]);
+    a * b / gcd_of_two_numbers(a, b)
+}
+
+fn gcd_of_two_numbers(a: i64, b: i64) -> i64 {
+    if b == 0 {
+        return a;
+    }
+    gcd_of_two_numbers(b, a % b)
 }
