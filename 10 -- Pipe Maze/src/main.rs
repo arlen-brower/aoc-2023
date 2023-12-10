f157ca0532d73058fe17a14b9c98acaf0df65885
Arlen <arlen.brower@curtin.edu.au>
2023-12-10 20:33:10 +0800

    Day 10 -- Closer...

--- 10 -- Pipe Maze/src/main.rs
+++ 10 -- Pipe Maze/src/main.rs
@@ -21,8 +21,9 @@ fn main() {
 
     let area: i32 = (rows * cols).try_into().unwrap();
 
-    println!("Total cells: {}", area);
-    println!("{}", area - do_maze(lines, rows, cols));
+    // println!("Total cells: {}", area);
+    // println!("{}", area - do_maze(lines, rows, cols));
+    do_maze(lines, rows, cols);
 }
 fn do_maze(lines: Vec<&str>, rows: usize, cols: usize) -> i32 {
     let mut char_map: HashMap<(usize, usize), char> = HashMap::new();
@@ -42,14 +43,22 @@ fn do_maze(lines: Vec<&str>, rows: usize, cols: usize) -> i32 {
     let mut right: (usize, usize);
     let mut left: (usize, usize);
 
+    let mut upl: (usize, usize);
+    let mut upr: (usize, usize);
+    let mut dl: (usize, usize);
+    let mut dr: (usize, usize);
+
+    start = (1, 1);
+
     // TODO
-    char_map.insert(start, '|');
+    // char_map.insert(start, '|');
 
     let mut nodes: VecDeque<((usize, usize), u32)> = VecDeque::new();
     nodes.push_back((start, 0));
 
     let mut dists: Vec<u32> = Vec::new();
     let mut dist_map: HashMap<(usize, usize), char> = HashMap::new();
+    let mut pipe_map: HashMap<(usize, usize), char> = HashMap::new();
     let mut visited: Vec<(usize, usize)> = Vec::new();
     visited.push(start);
 
@@ -60,49 +69,107 @@ fn do_maze(lines: Vec<&str>, rows: usize, cols: usize) -> i32 {
         down = (cur_pos.0 + 1, cur_pos.1);
         right = (cur_pos.0, cur_pos.1 + 1);
         left = (cur_pos.0, cur_pos.1 - 1);
+
+        upl = (cur_pos.0 - 1, cur_pos.1 - 1);
+        upr = (cur_pos.0 - 1, cur_pos.1 + 1);
+        dl = (cur_pos.0 + 1, cur_pos.1 - 1);
+        dr = (cur_pos.0 + 1, cur_pos.1 + 1);
+
         dists.push(cur_dist);
-        dist_map.insert(cur_pos, cur_dist.to_string().chars().nth(0).unwrap());
+
         let c_up = char_map.get(&up).unwrap_or(&' ');
         let c_down = char_map.get(&down).unwrap_or(&' ');
         let c_right = char_map.get(&right).unwrap_or(&' ');
         let c_left = char_map.get(&left).unwrap_or(&' ');
         let c_cur = char_map.get(&cur_pos).unwrap();
+        pipe_map.insert(cur_pos, *c_cur);
+        dist_map.insert(cur_pos, cur_dist.to_string().chars().nth(0).unwrap());
+
+        let c_upl = char_map.get(&upl).unwrap_or(&' ');
+        let c_upr = char_map.get(&upr).unwrap_or(&' ');
+        let c_dl = char_map.get(&dl).unwrap_or(&' ');
+        let c_dr = char_map.get(&dr).unwrap_or(&' ');
 
         let mut neighbours: Vec<&char> = Vec::new();
 
-        if connected(*c_cur, *c_up, 'n') && !visited.contains(&up) {
+        if can_squeeze(&char_map, cur_pos, *c_up, 'n') && !visited.contains(&up) {
             // if *c_up == '.' && !(visited.contains(&up)) {
             nodes.push_back((up, cur_dist + 1));
             visited.push(up);
             neighbours.push(c_up);
         }
-        if connected(*c_cur, *c_down, 's') && !visited.contains(&down) {
+        if can_squeeze(&char_map, cur_pos, *c_down, 's') && !visited.contains(&down) {
             // if *c_down == '.' && !(visited.contains(&down)) {
             nodes.push_back((down, cur_dist + 1));
             visited.push(down);
             neighbours.push(c_down);
         }
-        if connected(*c_cur, *c_right, 'e') && !visited.contains(&right) {
+        if can_squeeze(&char_map, cur_pos, *c_right, 'e') && !visited.contains(&right) {
             // if *c_right == '.' && !(visited.contains(&right)) {
             nodes.push_back((right, cur_dist + 1));
             visited.push(right);
             neighbours.push(c_right);
         }
-        if connected(*c_cur, *c_left, 'w') && !visited.contains(&left) {
+        if can_squeeze(&char_map, cur_pos, *c_left, 'w') && !visited.contains(&left) {
             // if *c_left == '.' && !(visited.contains(&left)) {
             nodes.push_back((left, cur_dist + 1));
             visited.push(left);
             neighbours.push(c_left);
         }
+        if can_squeeze(&char_map, cur_pos, *c_upl, '7') && !visited.contains(&upl) {
+            nodes.push_back((upl, cur_dist + 1));
+            visited.push(upl);
+        }
+        if can_squeeze(&char_map, cur_pos, *c_upr, '9') && !visited.contains(&upr) {
+            nodes.push_back((upr, cur_dist + 1));
+            visited.push(upr);
+        }
+        if can_squeeze(&char_map, cur_pos, *c_dl, '1') && !visited.contains(&dl) {
+            nodes.push_back((dl, cur_dist + 1));
+            visited.push(dl);
+        }
+        if can_squeeze(&char_map, cur_pos, *c_dr, '3') && !visited.contains(&dr) {
+            nodes.push_back((dr, cur_dist + 1));
+            visited.push(dr);
+        }
+        // if connected(*c_cur, *c_up, 'n') && !visited.contains(&up) {
+        //     // if *c_up == '.' && !(visited.contains(&up)) {
+        //     nodes.push_back((up, cur_dist + 1));
+        //     visited.push(up);
+        //     neighbours.push(c_up);
+        // }
+        // if connected(*c_cur, *c_down, 's') && !visited.contains(&down) {
+        //     // if *c_down == '.' && !(visited.contains(&down)) {
+        //     nodes.push_back((down, cur_dist + 1));
+        //     visited.push(down);
+        //     neighbours.push(c_down);
+        // }
+        // if connected(*c_cur, *c_right, 'e') && !visited.contains(&right) {
+        //     // if *c_right == '.' && !(visited.contains(&right)) {
+        //     nodes.push_back((right, cur_dist + 1));
+        //     visited.push(right);
+        //     neighbours.push(c_right);
+        // }
+        // if connected(*c_cur, *c_left, 'w') && !visited.contains(&left) {
+        //     // if *c_left == '.' && !(visited.contains(&left)) {
+        //     nodes.push_back((left, cur_dist + 1));
+        //     visited.push(left);
+        //     neighbours.push(c_left);
+        // }
+
+        // Part 2 stuff
         // println!("({},{}): {:?}", cur_pos.0, cur_pos.1, neighbours);
     }
 
     // println!("{:?}", dists);
 
+    // Really, ground count now
     let mut pipe_count = 0;
     for r in 0..rows {
         for c in 0..cols {
-            if dist_map.get(&(r, c)).unwrap_or(&'.').is_numeric() {
+            if dist_map.get(&(r, c)).unwrap_or(&'.').is_numeric()
+                && *(char_map.get(&(r, c)).unwrap()) == '.'
+            {
                 pipe_count += 1;
             }
             print!("{}", dist_map.get(&(r, c)).unwrap_or(&'.'));
@@ -115,37 +182,108 @@ fn do_maze(lines: Vec<&str>, rows: usize, cols: usize) -> i32 {
     pipe_count
 }
 
+fn can_squeeze(
+    char_map: &HashMap<(usize, usize), char>,
+    cur_pos: (usize, usize),
+    dest: char,
+    dir: char,
+) -> bool {
+    let mut ret_val = false;
+    let mut up = (cur_pos.0 - 1, cur_pos.1);
+    let mut down = (cur_pos.0 + 1, cur_pos.1);
+    let mut right = (cur_pos.0, cur_pos.1 + 1);
+    let mut left = (cur_pos.0, cur_pos.1 - 1);
+
+    let mut upl = (cur_pos.0 - 1, cur_pos.1 - 1);
+    let mut upr = (cur_pos.0 - 1, cur_pos.1 + 1);
+    let mut dl = (cur_pos.0 + 1, cur_pos.1 - 1);
+    let mut dr = (cur_pos.0 + 1, cur_pos.1 + 1);
+    let c_up = char_map.get(&up).unwrap_or(&' ');
+    let c_down = char_map.get(&down).unwrap_or(&' ');
+    let c_right = char_map.get(&right).unwrap_or(&' ');
+    let c_left = char_map.get(&left).unwrap_or(&' ');
+    let c_cur = char_map.get(&cur_pos).unwrap();
+
+    let c_upl = char_map.get(&upl).unwrap_or(&' ');
+    let c_upr = char_map.get(&upr).unwrap_or(&' ');
+    let c_dl = char_map.get(&dl).unwrap_or(&' ');
+    let c_dr = char_map.get(&dr).unwrap_or(&' ');
+
+    if dest == ' ' {
+        return false;
+    }
+
+    if dest == '.' {
+        return true;
+    }
+
+    if dir == 'n' && dest != '-' {
+        if dest == '|' {
+            ret_val = true;
+        } else if dest == 'J' && *c_upl == 'L' {
+            ret_val = true;
+        } else if dest == 'L' && *c_upr == 'J' {
+            ret_val = true;
+        }
+    } else if dir == 's' && dest != '-' {
+        if dest == '|' {
+            ret_val = true;
+        } else if dest == 'F' && *c_dl == '7' {
+            ret_val = true;
+        } else if dest == '7' && *c_dr == 'F' {
+            ret_val = true;
+        }
+    } else if dir == 'e' && dest != '|' {
+        if dest == '-' {
+            ret_val = true;
+        } else if dest == 'F' && *c_upr == 'L' {
+            ret_val = true;
+        } else if dest == 'L' && *c_dr == 'F' {
+            ret_val = true;
+        }
+    } else if dir == 'w' && dest != '|' {
+        if dest == '-' {
+            ret_val = true;
+        } else if dest == '7' && *c_upl == 'J' {
+            ret_val = true;
+        } else if dest == 'J' && *c_dl == '7' {
+            ret_val = true;
+        }
+    } else if dir == '7' && dest != 
+    ret_val
+}
+
 fn connected(s: char, n: char, dir: char) -> bool {
     let mut ret_val = false;
 
     // Going North
     if dir == 'n' {
-        if s == '|' || s == 'L' || s == 'J' {
-            if n == '|' || n == '7' || n == 'F' {
+        if s == '.' || s == '|' || s == 'L' || s == 'J' {
+            if n == '.' || n == '|' || n == '7' || n == 'F' {
                 ret_val = true;
             }
         }
     }
     // Going East
     else if dir == 'e' {
-        if s == '-' || s == 'L' || s == 'F' {
-            if n == '-' || n == 'J' || n == '7' {
+        if s == '.' || s == '-' || s == 'L' || s == 'F' {
+            if n == '.' || n == '-' || n == 'J' || n == '7' {
                 ret_val = true;
             }
         }
     }
     // Going West
     else if dir == 'w' {
-        if s == '-' || s == '7' || s == 'J' {
-            if n == '-' || n == 'L' || n == 'F' {
+        if s == '.' || s == '-' || s == '7' || s == 'J' {
+            if n == '.' || n == '-' || n == 'L' || n == 'F' {
                 ret_val = true;
             }
         }
     }
     // Going South
-    else if dir == 's' {
-        if s == '|' || s == 'F' || s == '7' {
-            if n == '|' || n == 'L' || n == 'J' {
+    else if dir == 'n' {
+        if s == '.' || s == '|' || s == 'F' || s == '7' {
+            if n == '.' || n == '|' || n == 'L' || n == 'J' {
                 ret_val = true;
             }
         }
