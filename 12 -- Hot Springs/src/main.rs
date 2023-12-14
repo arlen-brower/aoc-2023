96b41a708e82e7e50125d20ce153fe62503af86f
Arlen <arlen.brower@curtin.edu.au>
2023-12-15 00:20:55 +0800

    Day 12 -- Part 1

--- 12 -- Hot Springs/src/main.rs
+++ 12 -- Hot Springs/src/main.rs
@@ -19,8 +19,8 @@ fn main() {
             nums.push(n.parse().unwrap());
         }
 
-        println!("{}  ||  {block}  {:?}", solve_block(block, &nums[..]), nums);
-        sum += solve_block(block.trim(), &nums[..]);
+        println!("{}  ||  {block}  {:?}", solve(block, &nums[..]), nums);
+        sum += solve(block.trim(), &nums[..]);
     }
     println!("{sum} arrangements");
 
@@ -30,13 +30,84 @@ fn main() {
     // println!("{}", solve_block("????.#...#...", &[4, 1, 1]));
     // println!("{}", solve_block("????.######..#####.", &[1, 6, 5]));
     // println!("{}", solve_block("?###????????", &[3, 2, 1]));
-    // println!("{}", solve_block(".?.????#??#", &[1, 1, 2]));
+    println!("{}", solve_block(".?.????#??#", &[1, 1, 2]));
     println!("{}", solve_block(".?.????????", &[1, 1, 2]));
+    println!("{}", solve_block("???????????", &[1, 1, 2]));
 }
+
+fn solve(block: &str, nums: &[usize]) -> usize {
+    if nums.len() == 0 {
+        if block.contains('#') {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    if block.len() == 0 {
+        return 0;
+    }
+
+    let next_ch = block.chars().nth(0).unwrap();
+    let next_num = nums[0];
+
+    let octothorpe = || {
+        let mut x = next_num;
+        while block.get(..x).is_none() {
+            x -= 1;
+        }
+
+        let this_group: String = (&block[..x]).to_string().replace("?", "#");
+
+        let mut test_string = String::new();
+
+        for _i in 0..next_num {
+            test_string.push('#');
+        }
+
+        if *this_group != test_string {
+            return 0;
+        }
+
+        if block.len() == next_num {
+            if nums.len() == 1 {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        let peek_ch = block.chars().nth(next_num).unwrap();
+        if peek_ch == '?' || peek_ch == '.' {
+            return solve(&block[next_num + 1..], &nums[1..]);
+        }
+
+        return 0;
+    };
+
+    let dot = || {
+        return solve(&block[1..], nums);
+    };
+
+    let out = match next_ch {
+        '#' => octothorpe(),
+        '.' => dot(),
+        '?' => dot() + octothorpe(),
+        _ => panic!(),
+    };
+
+    println!("{:?} {:?} {}", block, nums, out);
+    out
+}
+
+// Old code, adhoc weird logic...
 
 fn solve_block(block: &str, nums: &[usize]) -> usize {
     if nums.len() > 0 && block.len() < nums[0] {
         // println!("{}", nums[0]);
+        return 0;
+    }
+    if nums.len() == 0 && block.contains('#') {
         return 0;
     }
     if nums.len() == 0 {
@@ -69,35 +140,37 @@ fn solve_block(block: &str, nums: &[usize]) -> usize {
             }
         }
 
-        cur_win = &block[j..j + window];
         if cur_win.contains('.') {
             j += 1;
             continue;
         }
 
-        match block.chars().nth(j + window) {
-            Some('#') => {
-                j += 1;
-                continue;
-            }
-            Some(_) => (),
-            None => (),
-        };
+        // match block.chars().nth(j + window) {
+        //     Some('#') => {
+        //         j += 1;
+        //         continue;
+        //     }
+        //     Some(_) => (),
+        //     None => (),
+        // };
         if cur_win.contains('#') {
             let mut need_rewind = false;
+            let start = j;
             while j < len - window && cur_win.chars().last().unwrap() == '#' {
                 j += 1;
-                cur_win = &block[j..j + window];
+                cur_win = &block[start..j + window];
                 need_rewind = true;
             }
             if need_rewind {
-                j -= 1;
+                // j -= 1;
             }
+            let hash_count = cur_win
+                .chars()
+                .fold(0, |acc, ch| if ch == '#' { acc + 1 } else { acc });
+
+            let win_len = cur_win.len();
         }
 
-        if nums.len() == 1 && cur_win.len() == window && !cur_win.contains('.') {
-            return 1;
-        }
         match block.get(j + window + offset..) {
             Some(sub) => sum += solve_block(sub, &nums[1..]),
             None => (),
