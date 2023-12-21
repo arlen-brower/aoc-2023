a8dcdea594ad3ff43faa4c62f162192281cbedf8
Arlen <arlen.brower@curtin.edu.au>
2023-12-21 15:02:02 +0800

    Day 21 -- For visualisation of part 2. I guess it would technically solve the problem eventually... :)

--- 21 -- Step Counter/src/main.rs
+++ 21 -- Step Counter/src/main.rs
@@ -18,12 +18,12 @@ fn main() {
 }
 
 fn solve(contents: &str) {
-    let mut layout: HashMap<(usize, usize), char> = HashMap::new();
-    let mut steps: HashSet<(usize, usize, u8)> = HashSet::new();
+    let mut layout: HashMap<(i64, i64), char> = HashMap::new();
+    let mut steps: HashSet<(i64, i64, i64)> = HashSet::new();
     let mut row = 0;
     let mut col = 0;
 
-    let mut start: (usize, usize) = (0, 0);
+    let mut start: (i64, i64) = (0, 0);
 
     for line in contents.lines() {
         col = 0;
@@ -31,8 +31,9 @@ fn solve(contents: &str) {
             if ch == 'S' {
                 start = (row, col);
                 layout.insert((row, col), '.');
+            } else {
+                layout.insert((row, col), ch);
             }
-            layout.insert((row, col), ch);
             col += 1;
         }
         row += 1;
@@ -41,33 +42,68 @@ fn solve(contents: &str) {
     let max_rows = row;
     let max_cols = col;
 
-    let mut visited: HashSet<(usize, usize)> = HashSet::new();
-    let mut q: VecDeque<(usize, usize, u8)> = VecDeque::new();
+    let mut visited: HashSet<(i64, i64)> = HashSet::new();
+    let mut q: VecDeque<(i64, i64, i64)> = VecDeque::new();
 
-    q.push_back((start.0, start.1, 0));
-    steps.insert((start.0, start.1, 0));
+    q.push_back((start.0 as i64, start.1 as i64, 0));
+    steps.insert((start.0 as i64, start.1 as i64, 0));
 
-    let step_limit = 6;
+    let mut canvas_min_r: i64 = 0;
+    let mut canvas_min_c: i64 = 0;
+    let mut canvas_max_r: i64 = max_rows as i64;
+    let mut canvas_max_c: i64 = max_cols as i64;
+
+    let step_limit = 100;
     while let Some((r, c, step)) = q.pop_front() {
-        let mut n: Vec<(usize, usize)> = Vec::new();
+        let mut n: Vec<(i64, i64)> = Vec::new();
 
         if step > step_limit {
             continue;
         }
+
+        if r < canvas_min_r {
+            canvas_min_r = r;
+        }
+        if r > canvas_max_r {
+            canvas_max_r = r;
+        }
+        if c < canvas_min_c {
+            canvas_min_c = c;
+        }
+        if c > canvas_max_c {
+            canvas_max_c = c;
+        }
+
         // Neighbours
-        if r > 0 && *layout.get(&(r - 1, c)).unwrap() == '.' {
+        if *layout
+            .get(&(modulo(r - 1, max_rows), modulo(c, max_cols)))
+            .unwrap()
+            == '.'
+        {
             //Up
             n.push((r - 1, c));
         }
-        if r < max_rows - 1 && *layout.get(&(r + 1, c)).unwrap() == '.' {
+        if *layout
+            .get(&(modulo(r + 1, max_rows), modulo(c, max_cols)))
+            .unwrap()
+            == '.'
+        {
             //Down
             n.push((r + 1, c));
         }
-        if c > 0 && *layout.get(&(r, c - 1)).unwrap() == '.' {
+        if *layout
+            .get(&(modulo(r, max_rows), modulo(c - 1, max_cols)))
+            .unwrap()
+            == '.'
+        {
             //Left
             n.push((r, c - 1));
         }
-        if c < max_cols - 1 && *layout.get(&(r, c + 1)).unwrap() == '.' {
+        if *layout
+            .get(&(modulo(r, max_rows), modulo(c + 1, max_cols)))
+            .unwrap()
+            == '.'
+        {
             //Right
             n.push((r, c + 1));
         }
@@ -81,28 +117,45 @@ fn solve(contents: &str) {
         }
     }
 
-    let filtered_map: HashSet<(usize, usize)> = steps
+    let filtered_map: HashSet<(i64, i64)> = steps
         .iter()
         .filter(|(_, _, step)| step % 2 == 0)
         .map(|(r, c, _)| (*r, *c))
         .collect();
 
-    print_map(&layout, &filtered_map, max_rows, max_cols);
+    print_map(
+        &layout,
+        &filtered_map,
+        canvas_min_r,
+        canvas_min_c,
+        canvas_max_r,
+        canvas_max_c,
+        max_rows,
+        max_cols,
+    );
     println!("{}", filtered_map.len());
 }
 
+fn modulo(a: i64, b: i64) -> i64 {
+    ((a % b) + b) % b
+}
+
 fn print_map(
-    layout: &HashMap<(usize, usize), char>,
-    visited: &HashSet<(usize, usize)>,
-    max_rows: usize,
-    max_cols: usize,
+    layout: &HashMap<(i64, i64), char>,
+    visited: &HashSet<(i64, i64)>,
+    min_rows: i64,
+    min_cols: i64,
+    max_rows: i64,
+    max_cols: i64,
+    mr: i64,
+    mc: i64,
 ) -> () {
-    for r in 0..max_rows {
-        for c in 0..max_cols {
-            match layout.get(&(r, c)) {
+    for r in min_rows..max_rows {
+        for c in min_cols..max_cols {
+            match layout.get(&(modulo(r, mr), modulo(c, mc))) {
                 Some(ch) => {
                     if visited.contains(&(r, c)) {
-                        print!("O");
+                        print!(".");
                     } else {
                         print!("{ch}");
                     }
